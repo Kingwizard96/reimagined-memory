9f9840695a007de86c81736fb09979e17e6e291d
Kingwizard96 <calvarado1996@gmail.com>
2024-01-03 19:33:56 -0600

    <descriptive message here>

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/') 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/NasaApp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-module.exports = mongoose.connection;
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
