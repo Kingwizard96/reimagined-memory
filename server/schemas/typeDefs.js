e78d20735be787dbcf663ce895c59c6f545ad83a
Kingwizard96 <calvarado1996@gmail.com>
2024-01-05 03:25:50 -0600

    moved api call into server side

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,48 @@
+
 const typeDefs = `
+  
   type Query {
-    hello: String
-    # Add other query fields here
+    me: User
+    users: [User]
+    user(username: String!): User
+    getNasaData(startDate: String, endDate: String): NasaData
+    getNasaImages: [NasaImage]
   }
 
-  # Define other types if needed
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveNasaImage(title: String!, url: String!, date: String!): NasaImage
+    removeNasaImage(nasaImageId: ID!): User
+    # Add other mutations as needed
+  }
+
+  type NasaData {
+    data: String
+    headers: String
+    status: Int
+    statusText: String
+  }
+
+  type NasaImage {
+    id: ID!
+    title: String!
+    url: String!
+    date: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    savedNasaImages: [NasaImage]
+    imageCount: Int
+  }
 `;
 
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -3,39 +3,63 @@ require('dotenv').config();
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
+const { authMiddleware } = require('./utils/auth');
 const path = require('path');
-
-const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const { fetchNasaData } = require('./utils/nasaApi');
+
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+const { typeDefs, resolvers } = require('./schemas');
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
+  introspection: true,
 });
 
 const startApolloServer = async () => {
-    await server.start();
-    app.use(express.urlencoded({ extended: false }));
-    app.use(express.json());
+  await server.start();
 
-    app.use('/graphql', expressMiddleware(server));
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
-    if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../client/dist')));
+  // Endpoint for fetching NASA data
+  app.get('/api/nasa-data', async (req, res) => {
+    try {
+      // date logic
+      const today = new Date();
+      const endDate = today.toISOString().split('T')[0];
+      today.setDate(today.getDate() - 9);
+      const startDate = today.toISOString().split('T')[0];
 
-        app.get('*', (req, res) => {
-            res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-        });
+      const nasaData = await fetchNasaData(startDate, endDate);
+
+      res.json(nasaData);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
+  });
 
-    db.once('open', () => {
-        app.listen(PORT, () => {
-            console.log(`API server running on port ${PORT}!`);
-            console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-        });
+  app.use('/graphql', expressMiddleware(server, { context: authMiddleware }));
+
+  // Serve client/build as static assets in production
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
-}
+  }
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
 
 startApolloServer();
